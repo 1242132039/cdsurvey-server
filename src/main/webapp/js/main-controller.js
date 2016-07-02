504b940817ab33d5300d3be12fc887d81eac9104
1242132039 <fe.chow@foxmail.com>
2016-07-03 01:36:51 +0800

    update.

--- src/main/webapp/js/main-controller.js
+++ src/main/webapp/js/main-controller.js
@@ -1,20 +1,20 @@
 'use strict';
 
 appControllers.controller('MainController', ['$rootScope', '$scope', '$http', 'authorization',
-    function($rootScope, $scope, $http, authorization) {
+    function ($rootScope, $scope, $http, authorization) {
         $scope.status = 'running...';
         $scope.profile = authorization.profile;
         $scope.isAdmin = authorization.hasRealmRole('admin')
         $scope.isManager = authorization.hasRealmRole('portaluser')
-        
-        $scope.getContracts = function() {
-        	$http.get("http://clouddata.f3322.net:10320/api/contracts").success(function(data) {
-            	$scope.contracts = data;
+
+        $scope.getContracts = function () {
+            $http.get("api/contracts").success(function (data) {
+                $scope.contracts = data;
             });
         }
-        
-        $scope.logout = function() {
-        	authorization.logout();
+
+        $scope.logout = function () {
+            authorization.logout();
         }
     }
 ]);
